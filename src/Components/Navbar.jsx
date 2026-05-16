8e6a1b7bda192eb68f27ae3f66621d9e9119c089
jonayet-nur <jonayetnur@gmail.com>
2026-05-16 21:02:46 +0600

    Navbar,Banner and footer section complete

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between p-4">
+        <ul className="flex gap-5">
+            <li><Link href={"/"}>Home</Link></li>
+            <li><Link href={"/destinations"}>Destinations</Link></li>
+            <li><Link href={"/my-booking"}>My Bookings</Link></li>
+             <li><Link href={"/admin"}>Admin</Link></li>
+
+        </ul>
+        <div>
+            <Image src={"/assets/Wanderlast.png"} alt="" height={150} width={150}></Image>
+        </div>
+
+         <ul className="flex gap-5">
+            <li><Link href={"/profile"}>Profile</Link></li>
+            <li><Link href={"/login"}>Login</Link></li>
+            <li><Link href={"/sign-up"}>Sign Up</Link></li>
+            
+
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
