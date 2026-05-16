1c20f41b83df6a3116c7db387052a914c286d172
jonayet-nur <jonayetnur@gmail.com>
2026-05-16 23:47:16 +0600

    create a add-destination page and added form for add new travel pakage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
             <li><Link href={"/destinations"}>Destinations</Link></li>
             <li><Link href={"/my-booking"}>My Bookings</Link></li>
              <li><Link href={"/admin"}>Admin</Link></li>
+             <li><Link href={"/add-destination"}>Add Destination</Link></li>
 
         </ul>
         <div>
